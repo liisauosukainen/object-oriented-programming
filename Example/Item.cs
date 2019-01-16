afcde05c46517a01eb0cba9507d3b4c824ab84ce
liisauosukainen <liisa.uosukainen@saimia.fi>
2019-01-16 20:38:18 +0200

    Added Item class.

---
+++ Example/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Example
+{
+    class Item
+    {
+        public string name;
+        public int id;
+        private static int counter = 0;
+
+        public Item()
+        {
+            this.name = System.String.Empty;
+            this.id = counter++;
+        }
+
+        public Item(string name)
+        {
+            this.name = name;
+            this.id = counter++;
+        }
+
+        public override string ToString()
+        {
+            return this.id + " : "+ this.name;
+        }
+    }
+}
