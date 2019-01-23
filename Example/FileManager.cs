f097904cf88c3a28f886a19ee49cb9cc32bf0270
liisauosukainen <liisa.uosukainen@saimia.fi>
2019-01-23 16:00:07 +0200

    Exercise FileManager added.

---
+++ Example/FileManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Example
+{
+    public class WordListNotFoundException : Exception
+    {
+        public WordListNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class FileManager
+    {
+        private string filePath;
+
+        public FileManager(string filePath)
+        {
+            this.filePath = String.Empty;
+        }
+
+        public string ReadWords()
+        {
+            string content = String.Empty;
+
+            content = ReadFile();
+
+            return content;
+        }
+
+        private string ReadFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not available");
+            }
+
+            string directoryName = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileName(filePath);
+            string fileExtension = Path.GetExtension(filePath);
+
+            Console.WriteLine("directoryName: " + directoryName);
+            Console.WriteLine("fileName: " + fileName);
+            Console.WriteLine("fileExtension: " + fileExtension);
+
+            string fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+            return fileContent;
+        }
+    }
+}
