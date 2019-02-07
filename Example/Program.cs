a2eea5a45884e16913c9b5658d5c22c4c504d6c7
liisauosukainen <liisa.uosukainen@saimia.fi>
2019-02-07 20:20:47 +0200

    Added examples of Hashtable and Dictionary.

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Example
 {
@@ -13,6 +15,9 @@ namespace Example
             CreateObjectsExample();
             CreatePersonsExample();
             CompareItemsInArrayExample();
+            CheckItemsInListExample();
+            CheckItemsInHashtableExample();
+            CheckItemsInDictionaryExample();
 
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
@@ -71,6 +76,71 @@ namespace Example
             else
             {
                 Console.WriteLine(items[0].name + " != " + items[1].name);
+            }
+        }
+
+        static void CheckItemsInListExample()
+        {
+            Console.WriteLine("\nAdding objects to the List");
+            List<Item> items = new List<Item>();
+            Item hammer = new Item("vasara");
+            Item nail = new Item("naula");
+            items.Add(hammer);
+            items.Add(nail);
+
+            foreach (Item tool in items)
+            {
+                Console.WriteLine($"{tool.id}: {tool.name} in the List");
+            }
+
+            if (items.Contains(nail))
+            {
+                Console.WriteLine($"{nail.name} found in the List");
+            }
+
+            items.Remove(nail);
+
+            if (items.Contains(nail))
+            {
+                Console.WriteLine($"{nail.name} found in the List");
+            }
+            else
+            {
+                Console.WriteLine($"{nail.name} removed from the List");
+            }
+        }
+
+        static void CheckItemsInHashtableExample()
+        {
+            Console.WriteLine("\nAdding objects to the Hashtable");
+            Hashtable items = new Hashtable();
+            Item hammer = new Item("vasara");
+            Item nail = new Item("naula");
+
+            items.Add(hammer.name, hammer);
+            items.Add(nail.name, nail);
+
+            // Enumerate the hashtable
+            foreach (DictionaryEntry entry in items)
+            {
+                Console.WriteLine("{0} = {1}", entry.Key, entry.Value);
+            }
+        }
+
+        static void CheckItemsInDictionaryExample()
+        {
+            Console.WriteLine("\nAdding objects to the Dictionary");
+            Dictionary<string, Item> dictionary = new Dictionary<string, Item>();
+            Item hammer = new Item("vasara");
+            Item nail = new Item("naula");
+
+            dictionary.Add(hammer.name, hammer);
+            dictionary.Add(nail.name, nail);
+
+            // Enumerate the dictionary
+            foreach (KeyValuePair<string, Item> kvp in dictionary)
+            {
+                Console.WriteLine("{0} = {1}", kvp.Key, kvp.Value);
             }
         }
     }
